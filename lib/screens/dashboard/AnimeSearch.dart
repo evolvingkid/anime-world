191892a2258b11198b36261f41ace1cdf197d255
TROJANCODE <codeboycodes@gmail.com>
2020-11-11 19:38:39 +0530

    Anime search error

--- lib/screens/dashboard/AnimeSearch.dart
+++ lib/screens/dashboard/AnimeSearch.dart
@@ -1,13 +1,21 @@
+import 'package:animeworld/core/getx/animesState.dart';
+import 'package:animeworld/screens/dashboard/AnimeDetails.dart';
+import 'package:animeworld/widgets/AnimeItem.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class AnimeSearch extends StatelessWidget {
+  final AnimeState animeState = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: TextField(
+          onChanged: (val) {
+            animeState.filterData(searchTitle: val);
+          },
           textInputAction: TextInputAction.search,
           decoration: InputDecoration(
             hintText: "Search Animes",
@@ -18,7 +26,19 @@ class AnimeSearch extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: GetX<AnimeState>(
+        builder: (_controller) {
+          return ListView.builder(
+            itemBuilder: (ctx, index) {
+              if (_controller.animeFilter.length > 0) {
+                return AnimeItem(data: _controller.animeFilter[index]);
+              } else {
+                return Container();
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
