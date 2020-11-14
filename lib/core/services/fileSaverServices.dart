2d55007892d0445781c5e1f659dfcaf6affe806e
evolving_kid <48388666+evolvingkid@users.noreply.github.com>
2020-11-14 19:34:31 +0530

    bug fix on file save services

--- lib/core/services/fileSaverServices.dart
+++ lib/core/services/fileSaverServices.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart' as path_Provider;
 import 'package:permission_handler/permission_handler.dart';
+import 'package:crypto/crypto.dart' as crypto;
 import  'package:save_in_gallery/save_in_gallery.dart';
 
 class FileSaverServices {
@@ -11,17 +13,27 @@ class FileSaverServices {
 
   Future<void> externalApplicationDirSave(
       {String filenames, dynamic data}) async {
- 
+    var bytes = utf8.encode(filenames);
+    var hashData = crypto.md5.convert(bytes);
+
     if (await Permission.storage.request().isGranted) {
       try {
-    
+        Directory dir = await path_Provider.getExternalStorageDirectory();
+        String _downloadDir = dir.path;
+        final String _filePath = '$_downloadDir/$hashData.jpg';
+        File _file = File(_filePath);
+        var raf = _file.openSync(mode: FileMode.write);
+        raf.writeFromSync(data);
+        await raf.close();
+        print(_filePath);
+       
         List<Uint8List> bytesList = [];
         bytesList.add(data);
         final _imageSaver =  ImageSaver();
         final res = await _imageSaver.saveImages(imageBytes: bytesList);
-        print(res);
         return null;
       } catch (e) {
+        print(e);
         throw e;
       }
     }
