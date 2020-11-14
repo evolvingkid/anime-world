51632907a6278f9f92e4a02923a15911d7d7a2a5
evolving_kid <48388666+evolvingkid@users.noreply.github.com>
2020-11-14 19:17:30 +0530

    gallery Saver

--- lib/core/services/fileSaverServices.dart
+++ lib/core/services/fileSaverServices.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart' as path_Provider;
 import 'package:permission_handler/permission_handler.dart';
-import 'package:crypto/crypto.dart' as crypto;
+import  'package:save_in_gallery/save_in_gallery.dart';
 
 class FileSaverServices {
   Future<Directory> getApplicationDir() async {
@@ -11,30 +11,15 @@ class FileSaverServices {
 
   Future<void> externalApplicationDirSave(
       {String filenames, dynamic data}) async {
-    var bytes = utf8.encode(filenames);
-    var hashData = crypto.md5.convert(bytes);
-
+ 
     if (await Permission.storage.request().isGranted) {
       try {
-        Directory dir = await path_Provider.getExternalStorageDirectory();
-        List<FileSystemEntity> _dirs =
-            dir.parent.parent.parent.parent.listSync();
-
-        String _downloadDir = _dirs
-            .where((element) => element.path.contains('Download'))
-            .first
-            .path;
-
-        final String _filePath = '$_downloadDir/$hashData.jpg';
-
-        File _file = File(_filePath);
-        var raf = _file.openSync(mode: FileMode.write);
-        raf.writeFromSync(data);
-        await raf.close();
-
-        print(_filePath);
-        print('Sucess');
-
+    
+        List<Uint8List> bytesList = [];
+        bytesList.add(data);
+        final _imageSaver =  ImageSaver();
+        final res = await _imageSaver.saveImages(imageBytes: bytesList);
+        print(res);
         return null;
       } catch (e) {
         throw e;
